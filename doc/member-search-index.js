95b0704ef50790148d998c9f2dda230921a7c805
MatheusDev22 <matheusbsb95@gmail.com>
2022-01-10 21:15:27 -0300

    commit

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"CalculoIMC","l":"CalculoIMC()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"CalculoIMC","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ src/CalculoIMC.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * Essa classe � util para fazer o c�lculo do IMC
+ * 
+ * @author Ben�
+ *
+ */
+public class CalculoIMC {
+
+	/**
+	 *  O m�todo principal faz o c�lculo do IMC
+	 *  
+	 * @param args Receve a entrada de dados da linha de comando
+	 */
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Digite seu peso.....: ");
+		double peso = entrada.nextDouble();
+		
+	//	System.out.println();
+		System.out.print("Digite sua Altura...: ");
+		double altura = entrada.nextDouble();
+		
+		double imc = peso / (altura * altura);
+		System.out.printf("Seu IMC.............: %.2f%n" , imc);
+		
+	}
+}
